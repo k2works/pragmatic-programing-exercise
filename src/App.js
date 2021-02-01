5aacad3ffce61827c66df5a3ca17b660e517a1e7
k2works <kakimomokuri@gmail.com>
2021-02-01 15:12:53 +0900

    perf: メモ化によるパフォーマンス改善

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 export class App {
   constructor() {
-    document.getElementById("app").innerHTML = Fibonacci.calc(0);
+    document.getElementById("app").innerHTML = Fibonacci.calc(40);
   }
 }
 export class Fibonacci {
-  static calc(number) {
+  static calc(number, memo = []) {
+    if (memo[number]) return memo[number];
     if (number === 0) return 0;
     if (number === 1) return 1;
 
-    return this.calc(number - 2) + this.calc(number - 1);
+    memo[number] = this.calc(number - 2, memo) + this.calc(number - 1, memo);
+    return memo[number];
   }
 }
