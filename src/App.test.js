26661e6a278ea22a63c95978379baa78b56a9bdd
k2works <kakimomokuri@gmail.com>
2021-02-01 14:35:02 +0900

    test: 三角測量

--- src/App.test.js
+++ src/App.test.js
@@ -8,4 +8,8 @@ describe("フィボナッチ数列", () => {
   test("1を渡したら1を返す", () => {
     expect(Fibonacci.fib(1)).toEqual(1);
   });
+
+  test("2を渡したら1を返す", () => {
+    expect(Fibonacci.fib(2)).toEqual(1);
+  });
 });
