cdc8af07b83fb031dc1af398a4d866fd23f3db21
k2works <kakimomokuri@gmail.com>
2021-02-01 15:05:21 +0900

    refactor: 関数名の変更

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 export class App {
   constructor() {
-    document.getElementById("app").innerHTML = Fibonacci.fib(0);
+    document.getElementById("app").innerHTML = Fibonacci.calc(0);
   }
 }
 export class Fibonacci {
-  static fib(number) {
+  static calc(number) {
     if (number === 0) return 0;
     if (number === 1) return 1;
 
-    return this.fib(number - 2) + this.fib(number - 1);
+    return this.calc(number - 2) + this.calc(number - 1);
   }
 }
