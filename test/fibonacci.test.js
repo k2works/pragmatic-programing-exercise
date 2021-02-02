d5cb76d9b765864747182d26b8592f242c347581
k2works <kakimomokuri@gmail.com>
2021-02-02 14:05:48 +0900

    docs: テスト

--- test/fibonacci.test.js
+++ test/fibonacci.test.js
@@ -6,68 +6,70 @@ import { GeneralTerm } from "../src/domain/fibonacci/generalTerm";
 import { Service } from "../src/application/fibonacci/service";
 
 describe("フィボナッチ数列", () => {
-  let recursive;
-  let loop;
-  let generalTerm;
-  beforeEach(() => {
-    recursive = new ValueCommand(new Recursive());
-    loop = new ValueCommand(new Loop());
-    generalTerm = new ValueCommand(new GeneralTerm());
+  describe("ドメイン", () => {
+    let recursive;
+    let loop;
+    let generalTerm;
+    beforeEach(() => {
+      recursive = new ValueCommand(new Recursive());
+      loop = new ValueCommand(new Loop());
+      generalTerm = new ValueCommand(new GeneralTerm());
+    });
+
+    test.each([
+      [0, 0],
+      [1, 1],
+      [2, 1],
+      [3, 2],
+      [4, 3],
+      [5, 5],
+    ])("%iを渡したら%pを返す", (number, expected) => {
+      expect(recursive.exec(number)).toEqual(BigInt(expected));
+    });
+
+    test("大きな数値を計算する_再起処理による実装", () => {
+      expect(recursive.exec(40)).toEqual(BigInt(102334155));
+    });
+
+    test("大きな数値を計算する_ループ処理による実装", () => {
+      expect(loop.exec(40)).toEqual(BigInt(102334155));
+    });
+
+    test("大きな数値を計算する_一般項による実装", () => {
+      expect(generalTerm.exec(40)).toEqual(BigInt(102334155));
+    });
+
+    test("10までのフィボナッチ配列を返す", () => {
+      const command = new ListCommand(new ValueCommand(new Recursive()));
+      const result = command.exec(40);
+
+      expect(result[0]).toEqual(BigInt(0));
+      expect(result[result.length - 1]).toEqual(BigInt(102334155));
+    });
   });
 
-  test.each([
-    [0, 0],
-    [1, 1],
-    [2, 1],
-    [3, 2],
-    [4, 3],
-    [5, 5],
-  ])("%iを渡したら%pを返す", (number, expected) => {
-    expect(recursive.exec(number)).toEqual(BigInt(expected));
-  });
+  describe("アプリケーション", () => {
+    let service;
+    beforeEach(() => {
+      service = new Service();
+    });
 
-  test("大きな数値を計算する_再起処理による実装", () => {
-    expect(recursive.exec(40)).toEqual(BigInt(102334155));
-  });
+    test("再帰リストを返す", () => {
+      const result = service.recursiveList(5);
 
-  test("大きな数値を計算する_ループ処理による実装", () => {
-    expect(loop.exec(40)).toEqual(BigInt(102334155));
-  });
+      expect(result[5]).toEqual(BigInt(5));
+    });
 
-  test("大きな数値を計算する_一般項による実装", () => {
-    expect(generalTerm.exec(40)).toEqual(BigInt(102334155));
-  });
+    test("ループリストを返す", () => {
+      const result = service.loopList(5);
 
-  test("10までのフィボナッチ配列を返す", () => {
-    const command = new ListCommand(new ValueCommand(new Recursive()));
-    const result = command.exec(40);
+      expect(result[5]).toEqual(BigInt(5));
+    });
 
-    expect(result[0]).toEqual(BigInt(0));
-    expect(result[result.length - 1]).toEqual(BigInt(102334155));
-  });
-});
+    test("一般項リストを返す", () => {
+      const result = service.generalTermList(5);
 
-describe("フィボナッチ数列サービス", () => {
-  let service;
-  beforeEach(() => {
-    service = new Service();
-  });
-
-  test("再帰リストを返す", () => {
-    const result = service.recursiveList(5);
-
-    expect(result[5]).toEqual(BigInt(5));
-  });
-
-  test("ループリストを返す", () => {
-    const result = service.loopList(5);
-
-    expect(result[5]).toEqual(BigInt(5));
-  });
-
-  test("一般項リストを返す", () => {
-    const result = service.generalTermList(5);
-
-    expect(result[5]).toEqual(BigInt(5));
+      expect(result[5]).toEqual(BigInt(5));
+    });
   });
 });
