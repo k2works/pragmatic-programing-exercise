28f878dded3dbe3d7523261cd1efd303176da813
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-02-04 15:15:46 +0900

    test: フィボナッチ数列API

--- api/index.ts
+++ api/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { Fibonacci } from "./domain/fibonacci";
 const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/api/:number", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  const { number } = req.params;
   const fib = new Fibonacci();
-  res.send(fib.greeting());
+  const result = fib.calc(parseInt(number, 10));
+  res.send(result.toString());
 });
 
 module.exports = app;
--- api/test/index.test.ts
+++ api/test/index.test.ts
@@ -1,49 +1,20 @@
-import express from "express";
 import supertest from "supertest";
+const app = require("../index");
 
-describe("APIテスト", () => {
-  test("getの場合", async () => {
-    const app = express();
-    app.get(
-      "/",
-      async (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        res.write("hello");
-        res.end();
-      }
-    );
-
+describe("フィボナッチ数列API", () => {
+  test("0を渡したら0を返す", async () => {
     const request = supertest(app);
-    const response = await request.get("/");
+    const response = await request.get("/api/0");
 
     expect(response.status).toBe(200);
-    expect(response.text).toEqual("hello");
+    expect(response.text).toEqual("0");
   });
 
-  test("postの場合", async () => {
-    const app = express();
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-
-    app.post(
-      "/",
-      async (
-        req: express.Request,
-        res: express.Response,
-        next: express.NextFunction
-      ) => {
-        res.write(req.body.value);
-        res.end();
-      }
-    );
-
+  test("4を渡したら3を返す", async () => {
     const request = supertest(app);
-    const response = await request.post("/").send({ value: "text" });
+    const response = await request.get("/api/4");
 
     expect(response.status).toBe(200);
-    expect(response.text).toEqual("text");
+    expect(response.text).toEqual("3");
   });
 });
