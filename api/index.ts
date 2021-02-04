b73647ab85db69f92d3c4962bfaf65fd3bb8d119
Katuyuki Kakigi <kakimomokuri@gmail.com>
2021-02-04 17:35:06 +0900

    feat: フィボナッチ数列API

--- api/index.ts
+++ api/index.ts
@@ -10,4 +10,12 @@ app.get("/api/:number", (req, res) => {
   res.send(result.toString());
 });
 
+app.get("/api/list/:number", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  const { number } = req.params;
+  const command = new Fibonacci(new FibonacciRecursive());
+  const result = command.generateList(parseInt(number, 10));
+  res.send(result);
+});
+
 module.exports = app;
--- api/test/index.test.ts
+++ api/test/index.test.ts
@@ -17,4 +17,12 @@ describe("フィボナッチ数列API", () => {
     expect(response.status).toBe(200);
     expect(response.text).toEqual("3");
   });
+
+  test("5までのフィボナッチ数列を返す", async () => {
+    const request = supertest(app);
+    const response = await request.get("/api/list/5");
+
+    expect(response.status).toBe(200);
+    expect(response.text).toEqual("[0,1,1,2,3,5]");
+  });
 });
