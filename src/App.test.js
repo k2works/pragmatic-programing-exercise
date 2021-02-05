b7a1a48c83a2b153b517b0d92184c5ab17ebbe84
k2works <kakimomokuri@gmail.com>
2021-02-05 16:00:24 +0900

    test: EventEmitterを継承したTodoListモデル

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { EventEmitter } from "./EventEmitter";
+import { TodoItemModel } from "./model/TodoItemModel";
+import { TodoListModel } from "./model/TodoListModel";
 
 const spyLog = jest.spyOn(console, "log");
 spyLog.mockImplementation((x) => x);
@@ -13,5 +15,38 @@ describe("EvenEmitterの実行サンプル", () => {
     expect(console.log).toBeCalled();
     expect(spyLog.mock.calls[0][0]).toEqual("One!");
     expect(spyLog.mock.calls[1][0]).toEqual("Two!");
+  });
+});
+
+describe("TodoItemModelを利用するサンプルコード", () => {
+  test("それぞれIDは異なる", () => {
+    const item = new TodoItemModel({
+      title: "未完了のTodoアイテム",
+      completed: false,
+    });
+    const completedItem = new TodoItemModel({
+      title: "完了済みのTodoアイテム",
+      completed: true,
+    });
+
+    expect(item.id).not.toEqual(completedItem.id);
+  });
+});
+
+describe("TodoListModelを利用するサンプルコード", () => {
+  test("Todoリストにアイテムが増える", () => {
+    const todoListModel = new TodoListModel();
+    todoListModel.onChange(() => {
+      console.log("TodoListの状態が変わりました");
+    });
+    todoListModel.addTodo(
+      new TodoItemModel({
+        title: "新しいTodoアイテム",
+        completed: false,
+      })
+    );
+
+    expect(console.log).toBeCalled();
+    expect(todoListModel.getTotalCount()).toEqual(1);
   });
 });
