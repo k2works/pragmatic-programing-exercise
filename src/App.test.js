7082b70faa3c93adeac9e6dc12ffef54011a1e51
k2works <kakimomokuri@gmail.com>
2021-02-05 15:47:41 +0900

    test: EventEmitter

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,17 @@
-test("Hello", () => {
-  expect(1).toBe(1);
+import { EventEmitter } from "./EventEmitter";
+
+const spyLog = jest.spyOn(console, "log");
+spyLog.mockImplementation((x) => x);
+
+describe("EvenEmitterの実行サンプル", () => {
+  test("イベントをディスパッチする", () => {
+    const event = new EventEmitter();
+    event.addEventListener("test-event", () => console.log("One!"));
+    event.addEventListener("test-event", () => console.log("Two!"));
+    event.emit("test-event");
+
+    expect(console.log).toBeCalled();
+    expect(spyLog.mock.calls[0][0]).toEqual("One!");
+    expect(spyLog.mock.calls[1][0]).toEqual("Two!");
+  });
 });
